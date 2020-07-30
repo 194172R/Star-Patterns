3a3da3f764ae905cd434192b09b85ca5f46c8c31
Pulini Tilanka <pulinitilanka@gmail.com>
2020-07-30 21:31:13 +0530

    Hollow Inverted Mirrored Right Triangle Star Pattern Added

---
+++ 15_hollow_inverted_mirrored_right_triangle_star_pattern_code.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+void main()
+{
+	int x,y,z;
+	for(x=5;x>0;x--)
+	{
+		for(y=x;y>0;y--)
+		{
+			if(x<5 && x>2)
+			{
+				if(y==x || y==1)
+				{
+					printf("*");
+				}
+				else
+				{
+					printf(" ");
+				}
+			}
+			else
+			{
+				printf("*");
+			}
+		}
+		printf("\n");
+	}
+}
