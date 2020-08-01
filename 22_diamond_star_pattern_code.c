a2328a7cec0b68179c9c9cd2856ae696797b4f7d
Pulini Tilanka <pulinitilanka@gmail.com>
2020-08-01 16:48:01 +0530

    Diamond Star Pattern Code Change

--- 22_diamond_star_pattern_code.c
+++ 22_diamond_star_pattern_code.c
@@ -1,5 +1,33 @@
 #include<stdio.h>
 void main()
 {
-	
+		int x,y,z=0;
+	for(x=10;x>0;x--)
+	{
+		if(x>5)
+		{
+			for(y=x-6;y>0;y--)
+			{
+				printf(" ");
+			}
+			for(y=z+1;y>0;y--)
+			{
+				printf("*");
+			}
+			z=z+2;
+		}
+		else
+		{
+			for(y=6-x;y>0;y--)
+			{
+				printf(" ");
+			}
+			for(y=z-3;y>0;y--)
+			{
+				printf("*");
+			}
+			z=z-2;
+		}
+		printf("\n");
+	}
 }
